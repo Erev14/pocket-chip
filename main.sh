81adb310aaf0b8ab6c8723cce8fd71b8ad0b07e5
Arevalo-Edgar14 <edgar.arevalo@alumnos.udg.mx>
2023-01-15 22:42:53 -0600

    feat: add stretch upgrade

--- main.sh
+++ main.sh
@@ -13,10 +13,12 @@ PREFERENCESFILE=`/etc/apt/preferences`
 JESSIEDIR=`$ACTUALDIR/jessie`
 STRETCHDIR=`$ACTUALDIR/stretch`
 BUSTERDIR=`$ACTUALDIR/buster`
+NETWORKMANAGERDIR=`/etc/NetworkManager/NetworkManager.conf`
+XORGDIR=`/etc/X11/xorg.conf`
 
 reset
 
-$POCKETDESKDIR/greetings.sh
+$POCKETDESKDIR/greeting.sh
 
 # set up jessie source list
 mv $SOURCESLISTFILE $SOURCESLISTFILE.bk
@@ -30,19 +32,23 @@ mv $PREFERENCESFILE $PREFERENCESFILE.bk
 
 cp $JESSIEDIR/preferences $PREFERENCESFILE
 
+sudo apt update && sudo apt upgrade
+
 apt install openssh-server x11vnc
-
-systemctl start ssh
-systemctl enable ssh
-
-# set up Stretch source list
-
-# set up Buster source list
-
-
-
 x11vnc -storepasswd
 nohup x11vnc -forever -nevershared -rfbauth ~/.vnc/passwd &
 
 wget www.lexaloffle.com/dl/chip/pico-8_0.2.5c_chip.zip
 sudo unzip pico-8_0.2.5c_chip.zip -d /usr/lib
+
+systemctl start ssh
+systemctl enable ssh
+
+# set up Stretch source list
+cp $STRETCHDIR/sources.list $SOURCESLISTFILE
+
+sudo apt update && sudo apt full-upgrade
+cp $STRETCHDIR/NetworkManager.conf $NETWORKMANAGERDIR
+cp $STRETCHDIR/xorg.conf $XORGDIR
+
+# set up Buster source list
